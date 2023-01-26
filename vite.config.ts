b1e1defb8a1758d611a2454412521ff55e5d4ca1
Francesc Vila <francescvilasubias@gmail.com>
2023-01-26 09:28:34 +0100

    fix: vite.config import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@views': path.resolve(__dirname, './src/views'),
+      '@interfaces': path.resolve(__dirname, './src/interfaces'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@src': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react()],
 });
